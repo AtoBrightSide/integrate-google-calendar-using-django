3665045d1a7d8a718071a2decd0a12f6b23309ac
Abel Mesele <se.abel.mesele@gmail.com>
2023-05-28 10:57:01 +0300

    refractored some code

--- calendar_main/calendar_integration/views.py
+++ calendar_main/calendar_integration/views.py
@@ -6,13 +6,12 @@ import requests
 
 class GoogleCalendarInitView(View):
     def get(self, request):
-        client_id = settings.CLIENT_ID
-        redirect_uri = settings.REDIRECT_URI
-        print(f"client id is -> {client_id}\nredirect uri -> {redirect_uri}")
-        scope = 'https://www.googleapis.com/auth/calendar'
-        
+        SCOPE = 'https://www.googleapis.com/auth/calendar'
+        AUTHORIZATION_ENDPOINT = 'https://accounts.google.com/o/oauth2/auth?client_id={}&redirect_uri={}&scope={}&response_type=code'
+
         # redirect the user to the Google authorization endpoint
-        auth_url = f'https://accounts.google.com/o/oauth2/auth?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&response_type=code'
+        auth_url = AUTHORIZATION_ENDPOINT.format(settings.CLIENT_ID, settings.REDIRECT_URI, SCOPE)
+
         return redirect(auth_url)
 
 class GoogleCalendarRedirectView(View):
@@ -29,20 +28,18 @@ class GoogleCalendarRedirectView(View):
             }
         )
         
-        # parse access token
+        # extract token
         access_token = response.json().get('access_token')
         
         calendar_id = 'primary'
         
-        # make get request google-calendar API
+        # make get request google-calendar API with token
         response = requests.get(
             f'https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events',
             headers={
                 'Authorization': f'Bearer {access_token}',
             }
         )
-        print(response.json())
-        
         events = response.json().get('items', [])
         
         return JsonResponse(events, safe=False)
